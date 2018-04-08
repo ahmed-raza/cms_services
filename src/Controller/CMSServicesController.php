3387f1483c4f4a430cd0d18c5746968d94341ef1
Ahmed Raza <raza1778@gmail.com>
2018-04-08 18:55:36 +0500

    returning 404 if no nodes found

--- src/Controller/CMSServicesController.php
+++ src/Controller/CMSServicesController.php
@@ -18,28 +18,31 @@ class CMSServicesController extends ControllerBase {
    */
   public function content($type = null) {
     $nids = \Drupal::entityQuery('node')->condition('type', $type)->execute();
-    $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
-    $bundle_fields = \Drupal::entityManager()->getFieldDefinitions('node', $type);
+    if ($nids) {
+      $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
+      $bundle_fields = \Drupal::entityManager()->getFieldDefinitions('node', $type);
 
-    $data = [];
-    foreach ($nodes as $nid => $node) {
-      foreach ($bundle_fields as $field) {
-        if ($node->hasField($field->getName())) {
-          $data[$nid][$field->getName()] = $node->{$field->getName()}->getValue();
-          if ($field->getType() == 'entity_reference' && $field->getSetting('target_type') == 'taxonomy_term') {
-            foreach ($data[$nid][$field->getName()] as $key => $tid) {
-              $data[$nid][$field->getName()][$key] = ['name'=>taxonomy_term_load($tid['target_id'])->getName()];
+      $data = [];
+      foreach ($nodes as $nid => $node) {
+        foreach ($bundle_fields as $field) {
+          if ($node->hasField($field->getName())) {
+            $data[$nid][$field->getName()] = $node->{$field->getName()}->getValue();
+            if ($field->getType() == 'entity_reference' && $field->getSetting('target_type') == 'taxonomy_term') {
+              foreach ($data[$nid][$field->getName()] as $key => $tid) {
+                $data[$nid][$field->getName()][$key] = ['name'=>taxonomy_term_load($tid['target_id'])->getName()];
+              }
             }
           }
         }
       }
-    }
 
-    $response = [
-      'type' => $type,
-      'nodes' => $data,
-    ];
-    return new JsonResponse($response);
+      $response = [
+        'type' => $type,
+        'nodes' => $data,
+      ];
+      return new JsonResponse($response);
+    }
+    return new JsonResponse(['error'=>'404']);
   }
 
 }
